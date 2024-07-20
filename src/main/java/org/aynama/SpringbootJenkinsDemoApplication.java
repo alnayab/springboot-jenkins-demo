a51e62706a60e2766d0443bdac4a691303f3dc8f
alnayab <shaiknayab2020@gmail.com>
2024-07-21 00:14:09 +0530

    sys
    sys

--- src/main/java/org/aynama/SpringbootJenkinsDemoApplication.java
+++ src/main/java/org/aynama/SpringbootJenkinsDemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootJenkinsDemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args)  {
+		System.out.println("@ main method...");
 		SpringApplication.run(SpringbootJenkinsDemoApplication.class, args);
 	}
 
