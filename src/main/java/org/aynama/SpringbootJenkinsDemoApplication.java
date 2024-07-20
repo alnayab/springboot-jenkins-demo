33a9de2517a442f2af68330e01e70cb905f870ad
aynama <nayab.aynama@gmail.com>
2024-07-20 22:32:21 +0530

    first commit

---
+++ src/main/java/org/aynama/SpringbootJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package org.aynama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/aynama/SpringbootJenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aynama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
